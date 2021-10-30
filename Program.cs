f5f6b7a3ac4dd45563d7d0c471f14aebdc5f4081
muscaxx <81636014+muscaxx@users.noreply.github.com>
2021-10-30 23:10:02 +0300

    Dosyalar

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsolll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kurları tanımladık
+            float dolar = 9.61f;
+            float euro = 11.10f;
+            float sterlin = 13.15f;
+            //kurlardan ne kadar alınacağını hesaplayacağız
+            int al_dolar;
+            int al_euro;
+            int al_sterlin;
+            //Toplam fiyatı tanımlıyoruz
+            float toplamfiyat;
+
+            Console.WriteLine("Dolar =" + dolar + "\n" + "Euro =" + euro + "\n" + "Sterlin =" + sterlin);
+            Console.WriteLine("Bankaya hoş geldiniz..Dolar,Euro,Sterlinden ne kadar almak istersiniz");
+            Console.WriteLine("Dolar = ");
+            al_dolar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Euro = ");
+            al_euro = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sterlin = ");
+            al_sterlin = Convert.ToInt32(Console.ReadLine());
+            toplamfiyat = al_dolar * dolar + al_euro*euro + al_sterlin*sterlin ;
+            Console.WriteLine("\nToplam Ücret = " + toplamfiyat + "TL");
+            Console.ReadLine();//kodumuz hemen kapanmasın diye bu komutu kullandık.
+        }
+    }
+}
